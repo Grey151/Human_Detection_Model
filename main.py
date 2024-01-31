df8fa2693fe2da1e8781f340ec93f26178f37543
Grey151 <125654489+Grey151@users.noreply.github.com>
2024-01-31 19:31:17 +0100

    Update main.py
    spelling error in first line
    from tenserflow -> tensorflow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tenserflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 from take_photo import capture_photo
 from crop import crop_and_save_images
 from human_detection_model import predict_image
